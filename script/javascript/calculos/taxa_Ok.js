ebf86aea7a2fc8761f112446f7f8389abf4202fe
Rafa MZ <mz.tears.of.time@gmail.com>
2025-03-10 21:54:20 -0300

    10. Add: percentual de conclusão de questões na lista

---
+++ script/javascript/calculos/taxa_Ok.js
@@ -0,0 +1,21 @@
+// Exibe a quantidade de itens concluídos, o total de itens e a taxa percentual de conclusão.
+
+function calcularPercentual()
+{
+	document.querySelectorAll ( "span[data-num1][data-num2]" ).forEach ( span =>
+	{
+		let num1 = parseFloat ( span.getAttribute ( "data-num1" ));
+		let num2 = parseFloat ( span.getAttribute ( "data-num2" ));
+
+		if ( !isNaN ( num1 ) && !isNaN ( num2 ) && num2 !== 0 )
+		{
+			let percentual = ( num1 / num2 ) * 100;
+			span.innerHTML = `( ${num1} / ${num2} ) ${percentual.toFixed(2)}% <i class="fas fa-check-circle" style="color: green; margin-right: 5px;"></i>`;
+		}
+
+		else
+		span.innerHTML = `<i class="fas fa-exclamation-circle" style="color: #93b079; margin-right: 5px;"></i> Erro`;
+	});
+}
+
+calcularPercentual();
